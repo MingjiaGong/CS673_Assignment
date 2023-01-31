4b2c0f71c3849ccbf4ea8cd79fa637bed4ec40b0
mingjiagong <gongmingjia233@gmail.com>
2023-01-30 21:57:46 -0500

    third commit

--- src/main/java/Is_Leap_Year.java
+++ src/main/java/Is_Leap_Year.java
@@ -9,7 +9,9 @@ public class Is_Leap_Year {
     }
 
     public static boolean Is_Leap_Year(int n) {
-       if(n%4 == 0 && n%100 != 0){
+       if(n%4 != 0){
+           return false;
+       } else if(n%4 == 0 && n%100 != 0){
            return true;
        }else if(n%100 == 0 && n%400 != 0){
            return false;
